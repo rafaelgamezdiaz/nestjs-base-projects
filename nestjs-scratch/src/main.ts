9a29b297645a4f1acba448576c21b7eba44c6e89
Rafael Gamez Diaz <rafaelgamezdiaz@gmail.com>
2022-04-17 11:16:41 -0400

    first commit

---
+++ nestjs-scratch/src/main.ts
@@ -0,0 +1,25 @@
+import {Controller, Get, Module} from "@nestjs/common";
+import {NestFactory} from "@nestjs/core";
+
+@Controller()
+export class AppController {
+    @Get()
+    index() {
+        return 'Hola NestJs';
+    }
+}
+
+@Module({
+    imports: [],
+    controllers: [AppController],
+    providers:[],
+    exports:[]
+})
+export class AppModule {}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+
+bootstrap().then(r => r);
